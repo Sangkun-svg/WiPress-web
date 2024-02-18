8d1a3209b4c765857aa83315118c6b5c97c9d2b0
Sangkun-svg <gims4752@gmail.com>
2024-02-18 17:02:33 +0900

    feat : implement Info page

---
+++ src/components/PostItems/InfoPostItem.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+interface Props {}
+
+const InfoPostItem = () => {
+  return (
+    <Container>
+      <ColDiv>
+        <PostTitle>게시판 글 제목입니다</PostTitle>
+        <PostDescription>
+          동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화
+          삼천리 화려강산 대한사람 대한으로 길이 보전하세
+        </PostDescription>
+      </ColDiv>
+      {/* TODO: Turn ON/OFF icon according data */}
+    </Container>
+  );
+};
+
+export default InfoPostItem;
+
+const Container = styled.div`
+  display: flex;
+  padding: 8px;
+  /* TODO: Change Padding Value accoding to has ImageURL  */
+  /* ImageURL ? 8px : 20px 18px */
+  border-radius: 6px;
+  background: #f7f7fa;
+  gap: 10px;
+`;
+
+const ColDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const PostTitle = styled.p`
+  color: #000;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%;
+`;
+const PostDescription = styled.p`
+  color: #222;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
---
+++ src/pages/info.tsx
@@ -0,0 +1,57 @@
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import InfoPostItem from "../components/PostItems/InfoPostItem";
+
+interface Props {}
+
+const InfoPage = () => {
+  return (
+    <Layout>
+      <Container>
+        <RowDiv>
+          <Title>여론분석 게시판</Title>
+          <SearchIcon />
+        </RowDiv>
+        {/* TODO: Implement OnClick event and search Page */}
+        <PostItemList>
+          <InfoPostItem />
+          <InfoPostItem />
+          <InfoPostItem />
+          <InfoPostItem />
+        </PostItemList>
+      </Container>
+    </Layout>
+  );
+};
+
+export default InfoPage;
+
+const Container = styled.div`
+  width: 100%;
+  margin-top: 26px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PostItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const RowDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  margin-bottom: 14px;
+`;
+
+const Title = styled.p`
+  color: #000;
+  font-size: 19px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 100%;
+`;
