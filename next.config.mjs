c07131430b4b46521a13043da1cb8b872a53afb1
Sangkun-svg <gims4752@gmail.com>
2024-02-18 17:01:51 +0900

    build : update next config image form

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,12 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
-    domains: ["s3-alpha-sig.figma.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-alpha-sig.figma.com",
+      },
+    ],
   },
 };
 
