8c8036269f4958c3ecb52a2acd288cfa540bcaeb
Sangkun-svg <gims4752@gmail.com>
2024-03-06 03:12:33 +0900

    feat : admin article

--- src/pages/admin/article/index.tsx
+++ src/pages/admin/article/index.tsx
@@ -1,23 +1,26 @@
 import { supabase } from '@/utils/database';
 import { Stack } from '@mui/material';
 import styled from 'styled-components';
-import { AdminLayout,AdminListItem } from '@/components/admin';
+import { AdminLayout,AdminArticleListItem } from '@/components/admin';
 
 export const getServerSideProps = async () => { 
-    const { data, error } = await supabase.from("Post").select().eq("type","article");
+    const { data, error } = await supabase
+    .from("Post")
+    .select("*")
+    .eq("type","article");
+    console.log({data})
     if(error) throw new Error(error.message);
     return { props: {
         article: data,
     } } 
 }
 
-const AdminArticle = ({props}:any) => {
-    console.log(props)
+const AdminArticle = (props:any) => {
     return (
         <AdminLayout>
             <Stack spacing={2}>
                 {props && props.article.map((el:any) => {
-                    return <AdminListItem post={el} type={"article"}/>
+                    return <AdminArticleListItem key={el.id} post={el} type={"article"}/>
                 })}
             </Stack>
         </AdminLayout>
