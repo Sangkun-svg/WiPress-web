1bd9f0045ec1b42c0910bb380b4bea3a6b47d0e3
Sangkun-svg <gims4752@gmail.com>
2024-03-06 02:53:04 +0900

    build : set envs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_STORAGE_URL: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL,
+    NEXT_PUBLIC_NEXTAUTH_SECRET_KEY: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
